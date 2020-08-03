a71e4f24fa4ed27fe9acd06411e843dbb894052c
Adam <62260663+Vealtex@users.noreply.github.com>
2020-08-03 22:34:26 +0200

    Added to github
    Yeah euhm?!

---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- Made by Adam, please leave the credits from Vealtex-->
+<!-- https://github.com/vealtex/MCWebsite -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VealtexCMS - V1</title>
+	<meta name="description" content="Write your description here if you have this CMS. And here you ip 'play.jeserver.net.">
+	<meta name="keywords" content="Type tags here for example #VealtaxCMS">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<script src="js/mc-player-counter.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="logo">
+			<!-- If you have the CMS put your logo in the /img folder. -->
+			<!-- Call it 'logo.png'! -->
+			<img src="img/logo.png" alt="Vealtex logo">
+		</div>
+
+		<div class="playercount">
+			<!-- Change play.yourserver.net with your own ip! -->
+			<!-- Make sure you do it for both examples below. -->
+			<p>Join us <span data-playercounter-ip="play.yourserver.net">
+			0</span> other players on <span>play.yourserver.net</span></p>
+		</div>
+
+		<div class="items">
+			<div class="item forums">
+				<!-- Change # with your forum URL-->
+				<a href="#"><img src="img/forums.png" alt="Minecraft forums icon" class="img"></a>
+			</div>
+
+			<div class="item store">
+				<!-- Change # with store URL -->
+				<a href="#"><img src="img/store.png" alt="Minecraft store icon" class="img"></a>
+			</div>
+
+			<div class="item vote">
+				<!-- Change # with your vote URL -->
+				<a href="#"><img src="img/vote.png" alt="Minecraft voting icon" class="img"></a>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+	<script src="js/firefly.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+</body>
+</html>
